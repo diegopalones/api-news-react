89cc8184172a8b87d7dfac545ae4b4cd9ac2c0aa
diegopalones <diego.palones@gmail.com>
2022-11-26 21:33:09 +0100

    styling header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
 
+
 const Header = () => {
   return (
     <div class="topnav">
       <div className="Reactnews">
+      
         <h2>New York Times News</h2>
       </div>
       <div>
         <span>
-          <Link to="/">Home</Link>
+          <Link to="/">Home</Link> 
         </span>
         <span>
           <Link to="/form">Form</Link>
